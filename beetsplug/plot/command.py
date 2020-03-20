b03450566e0eb08d09e680d3eb6052228b677de2
Adam Jakab <jakabadambalazs@gmail.com>
2020-03-21 00:34:07 +0100

    table and plot are working - hardcoded fields

--- beetsplug/plot/command.py
+++ beetsplug/plot/command.py
@@ -8,9 +8,12 @@
 
 from optparse import OptionParser
 import numpy as np
+import pandas as pd
 import termplotlib as tpl
+import termtables as tt
 
 from beets import library
+from beets.dbcore import types
 from beets.dbcore.db import Results
 from beets.dbcore.queryparse import parse_query_part
 from beets.library import Library, Item, parse_query_string
@@ -59,52 +62,147 @@ class PlotCommand(Subcommand):
         self.handle_display()
 
     def handle_display(self):
-        field_to_examine = self.query.pop(0)
-        items = self._retrieve_library_items()
-        self.plotit(items, field_to_examine)
+        field_to_examine = "genre"
+        # field_to_examine = self.query.pop(0)
 
-    def plotit(self, items, field):
-        sample = []
+        fields = ["id", "bpm", "year", "country", "acoustid_id", "mood_aggressive", field_to_examine]
+        lib_items = self._retrieve_library_items()
+        data = self._extract_data_from_items(lib_items, fields)
+
+        self.print_describe_table(data, field_to_examine)
+        self.plotit(data, field_to_examine)
+
+    def _describe(self, data, field):
+        desc = {}
+
+        df = pd.DataFrame(data)
+        field_vector = df[field]
+        uc: pd.Series = field_vector.value_counts(sort=True, dropna=False)
+
+        # Field name
+        desc["field_name"] = {'label': 'Field name', 'value': field}
+
+        # Field type
+        field_type = self._get_field_type(field)
+        desc["field_type"] = {'label': 'Field type', 'value': field_type}
+
+        # Total count
+        total_count = df[field].count()
+        desc["total_count"] = {'label': 'Count', 'value': total_count}
+
+        # Min
+        if field_type in self._get_dbcore_numeric_types():
+            min = field_vector.min()
+            desc["min"] = {'label': 'Min', 'value': min}
+
+        # Max
+        if field_type in self._get_dbcore_numeric_types():
+            max = field_vector.max()
+            desc["max"] = {'label': 'Max', 'value': max}
+
+        # Mean
+        if field_type in self._get_dbcore_numeric_types():
+            mean = field_vector.mean()
+            desc["mean"] = {'label': 'Mean', 'value': mean}
+
+        # Median
+        if field_type in self._get_dbcore_numeric_types():
+            median = field_vector.median()
+            desc["median"] = {'label': 'Median', 'value': median}
+
+        if field_type in self._get_dbcore_numeric_types():
+            null_count = (df[field].isna()).sum()
+        else:
+            null_count = (df[field] == '').sum()
+        desc["null_count"] = {'label': 'Empty', 'value': null_count}
+
+        # Unique count
+        unique_count = uc.count()
+        desc["unique_count"] = {'label': 'Unique', 'value': unique_count}
+
+        # Unique First
+        unique_keys = list(uc.keys())
+        unique_first_key = unique_keys[0]
+        unique_first_val = uc.max()
+        unique_first_str = "{}({})".format(unique_first_key, unique_first_val)
+        desc["unique_first"] = {'label': 'Most frequent', 'value': unique_first_str}
+
+        # Unique First
+        unique_last_key = unique_keys[-1]
+        unique_last_val = uc.min()
+        unique_last_str = "{}({})".format(unique_last_key, unique_last_val)
+        desc["unique_last"] = {'label': 'Least frequent', 'value': unique_last_str}
+
+        return desc
+
+    def print_describe_table(self, data, field):
+        desc = self._describe(data, field)
+
+        table_data = []
+        for key in desc:
+            data = desc[key]
+            table_data.append([data["label"], data["value"]])
+
+        tt.print(
+            table_data,
+            header=["Name", "Value"],
+            padding=(0, 1),
+            alignment="lr"
+        )
+
+    def plotit(self, data, field):
+        df = pd.DataFrame(data, columns=["id", field])
+
+        uc: pd.Series = df[field].value_counts(sort=True, dropna=False)
+
+        self._say("Plotting attribute: {}".format(field), log_only=False)
+        fig = tpl.figure()
+        fig.barh(uc.values, list(uc.keys()), force_ascii=False)
+        fig.show()
+
+    def _get_dbcore_numeric_types(self):
+        type_classes = []
+
+        dbcore_types = [
+            types.Integer,
+            types.Float,
+            types.NullFloat,
+            types.PaddedInt,
+            types.NullPaddedInt,
+            types.ScaledInt
+        ]
+
+        for dt in dbcore_types:
+            type_classes.append("{}.{}".format(dt.__module__, dt.__name__))
+
+        return type_classes
+
+    def _get_field_type(self, field):
+        fld_type = None
+
+        if field in Item._fields:
+            ft = Item._fields[field]
+            fld_type = "{}.{}".format(ft.__module__, ft.__class__.__name__)
+
+        return fld_type
+
+    def _extract_data_from_items(self, items, fields):
+        data = []
 
         for item in items:
-            if item.get(field):
-                sample.append(item.get(field))
+            item_data = {}
 
-        from collections import Counter
-        counter = Counter(sample)
+            for field in fields:
+                item_data[field] = item.get(field, default="")
 
-        lst = []
-        for item in counter.items():
-            lst.append(list(item))
+            data.append(item_data)
 
-        print(lst)
-
-        arr = np.array(lst)
-        print(arr)
-
-        # frequencies = list(counter.values())
-        # names = list(counter.keys())
-        # print(names)
-        # print(frequencies)
-
-        # arr.sort(axis=0)
-        # print(arr)
-
-        return
-        # sample = np.array(sample)
-        # print(sample)
-        # counts, bin_edges = np.histogram(sample, bins=10)
-
-        self._say("Displaying: {}".format(field), log_only=False)
-        fig = tpl.figure()
-        fig.barh(frequencies, names, force_ascii=False)
-        # fig.hist(counts, bin_edges, orientation="horizontal", force_ascii=False)
-        fig.show()
+        return data
 
     def _retrieve_library_items(self):
         full_query = self.query
         parsed_query = parse_query_string(" ".join(full_query), Item)[0]
-        self._say("Song selection query: {}".format(parsed_query), log_only=True)
+        self._say("Song selection query: {}".format(parsed_query), log_only=False)
 
         return self.lib.items(parsed_query)
 
